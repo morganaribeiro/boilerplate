a0c8f21b94148fb2c897a2ed4fdd3b9c2eb87607
Morgana Oliveira <morgana.ifce.2019@gmail.com>
2025-02-02 19:12:18 -0300

    chore(config): add RTL and first test

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,23 +3,35 @@ import pluginJs from '@eslint/js'
 import tseslint from 'typescript-eslint'
 import pluginReact from 'eslint-plugin-react'
 import prettierConfig from 'eslint-config-prettier'
-import prettierPlugin from 'eslint-plugin-prettier'
+import pluginPrettier from 'eslint-plugin-prettier'
+import pluginJest from 'eslint-plugin-jest'
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  { files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'] },
-  { languageOptions: { globals: globals.browser } },
-  pluginJs.configs.recommended,
-  ...tseslint.configs.recommended,
-  pluginReact.configs.flat.recommended,
   {
+    files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'],
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.jest
+      }
+    },
     plugins: {
-      prettier: prettierPlugin
+      prettier: pluginPrettier,
+      jest: pluginJest,
+      react: pluginReact
     },
     rules: {
       ...prettierConfig.rules,
       'react/react-in-jsx-scope': 'off',
-      'prettier/prettier': 'error'
+      'prettier/prettier': [
+        'error',
+        {
+          endOfLine: 'auto'
+        }
+      ]
     }
-  }
+  },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended
 ]
---
+++ src/components/Main/index.tsx
@@ -0,0 +1,9 @@
+const Main = () => (
+  <div>
+    <main>
+      <h2>React Avançado!</h2>
+    </main>
+  </div>
+)
+
+export default Main
